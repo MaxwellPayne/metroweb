ed6287bd0251bddddf747c6a50b9cd3079558963
Spencer Miskoviak <spencer@skovistudios.com>
2015-06-19 17:00:12 -0500

    removed excess database stuff

--- server.go
+++ server.go
@@ -1,34 +1,10 @@
 package main
 
 import (
-  "encoding/json"
-  "io/ioutil"
-  "fmt"
-  "github.com/Skovy/metroweb/database"
   "github.com/go-martini/martini"
 )
 
-type Configuration struct {
-  Db_user        string
-  Db_password    string
-  Db_host        string
-  Db_database    string
-}
-
-var conf Configuration
-
-// decode and load the config.json file
-func loadConfiguration() {
-
-  content, _ := ioutil.ReadFile("config.json")
-  json.Unmarshal(content, &conf)
-}
-
 func main() {
-
-  loadConfiguration()
-  fmt.Printf("%v", conf)
-  database.DatabaseConnect(conf.Db_user, conf.Db_password, conf.Db_host, conf.Db_database)
 
   m := martini.Classic()
   m.Get("/", func() string {
